3dbe038ae2a65a2a9e9b595a5583aba576c20760
Wojciech Sobasik <wojciech.sobasik@gmail.com>
2017-09-19 19:10:01 +0200

    zadanie domowe cd.

---
+++ src/Device.java
@@ -0,0 +1,27 @@
+public class Device {
+
+    String code;
+    String producer;
+    String type;
+    double price;
+
+
+    public Device(String code, String producer, String type) {
+        this.code = code;
+        this.producer = producer;
+        this.type = type;
+    }
+
+    public Device(String code, String producer, String type, double price) {
+        this.code = code;
+        this.producer = producer;
+        this.type = type;
+        this.price = price;
+    }
+
+
+    public String getInfo() {
+
+        return "Kod: " + code +", producent: "+producer+", typ: "+type+", cena: "+price;
+    }
+}
